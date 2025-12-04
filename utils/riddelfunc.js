dceda7ad61f84987fe4963dea1fd78837a0db397
Yechiel <springer058@gmail.com>
2025-12-04 12:29:50 +0200

    get reddel'

---
+++ utils/riddelfunc.js
@@ -0,0 +1,39 @@
+import * as ridarr from "../riddles/riddlesarr.js"
+import input from "analiza-sync";
+
+
+
+
+function askRiddle(riddleObj){
+    console.log(riddleObj.namer)
+    console.log(riddleObj.taskDescription)
+    let valid = false
+    while(valid == false){
+        if("choices" in riddleObj){
+            console.log(riddleObj.choices)
+            const choys = input("What is your choys? ");
+            if(choys == riddleObj.correctAnswer - 1){
+                valid = true
+            }else{
+                console.log("tray egen")
+
+                
+                continue}
+
+
+        }
+        if("choices" in riddleObj === false){
+            
+            const choys = input("What is your choys? ");
+            if(choys == riddleObj.correctAnswer){
+                valid = true
+            }else{
+                console.log("tray egen")
+
+                
+                continue}
+        }
+    }    
+
+
+    console.log("corect veri good")}
